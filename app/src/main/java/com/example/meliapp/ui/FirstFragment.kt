1421f717aa76824053157f5c36700d0683a6bfdb
ljirodrio <leon.rodriguez.ore@gmail.com>
2023-02-17 04:05:06 -0300

    se consume primer servicio de tipos de tarjeta se captura onclick en el tipo de tarjeta
    crear nuevo servicio de bancos según tipo de tarjeta
    
    se crea servicio de bancos , ahora obtener servicio de pago pasando el monto al servicio , pasar banco y tipo de pago
    
    tercer servicio consumido de forma correcta se le pasa id del banco , monto hardcode por ahora
    crear adapter para recomendar cuotas
    
    selección simple en el recycler view
    
    se pasa monto total a fragmnets

--- app/src/main/java/com/example/meliapp/ui/FirstFragment.kt
+++ app/src/main/java/com/example/meliapp/ui/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.example.meliapp.ui
 
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.HandlerCompat.postDelayed
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.viewpager2.widget.ViewPager2
@@ -43,8 +45,20 @@ class FirstFragment : Fragment() {
             payment = it.getString("payment")
             amount = it.getDouble("amount",0.0).toString()
         }
+
         if (arguments != null && !amount.equals("0.0")) {
-            Toast.makeText(context, amount + installments, Toast.LENGTH_LONG).show()}
+            //handler postdelayed para que se ejecute despues de que se cargue la vista
+            android.os.Handler(Looper.getMainLooper()).postDelayed({
+                Toast.makeText(
+                    context,
+                    "monto pagado : " + amount + "\n" +
+                            "Banco : " + namebank + "\n" +
+                            "Cuotas : " + installments+ "\n" +
+                            "Medio de pago : " + payment
+                    , Toast.LENGTH_LONG
+                ).show()
+            }, 800)
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
